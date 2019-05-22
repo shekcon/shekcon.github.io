346b68d236e56307d84d7def760b77f319846626
Shekcon <quangsang9773@gmail.com>
2019-05-23 03:34:56 +0700

    Summary:         - Add title, logo of pages         - Rewrite content of websites & add avatar         - Issued can show social icons of react-social-icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.jpeg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h3>
+          Lê Quang Sang
+        </h3>
+        <p className="App-content">
+        I’m passion about technologies, and one most of aspects is problem-solving. I love learning new things and facing interesting challenges.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
