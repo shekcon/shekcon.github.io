8aab11b13bde4586af406cc5ecfc50d0d86e1043
Shekcon <quangsang9773@gmail.com>
2019-05-23 21:49:37 +0700

    Updates

---
+++ precache-manifest.0af8332322aea5e37257dd40ac4ae3d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5427a8d2c1ccf966f82aa64d8f1af89d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d18edabf317b6f767c7",
+    "url": "/static/css/main.e9037c80.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1d18edabf317b6f767c7",
+    "url": "/static/js/main.971f2eed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
